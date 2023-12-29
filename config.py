a24416daf125ba7d59e6212ab023e0371e4aa107
Kuan002 <143362120+Kuan002@users.noreply.github.com>
2023-12-29 15:31:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee2.ctlxtqdmfm1u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee2"
+custombucket = "cheongok-employee"
+customregion = "us-east-1"
+
